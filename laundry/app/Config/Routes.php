05fefe48e54907e1498ef42066ef55b953b9b8ad
galih <maulana.galih.p@gmail.com>
2024-03-09 20:32:56 +0700

    Add registration and login
    Add registration and login

--- laundry/app/Config/Routes.php
+++ laundry/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('customer/register', 'customer\Register::register');
+$routes->get('customer/register', 'customer\Register::register');
+$routes->get('customer/login', 'customer\Login::login');
+$routes->post('customer/login', 'customer\Login::login');
